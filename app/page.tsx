c17487be5d3197a7133e78d32c20e5b4938d4ad2
Safi <139801311+safi-siddiqui-github@users.noreply.github.com>
2023-11-26 03:04:45 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex p-5">
       <h1 className="text-xl">
-        Hello docker Next Js 14
+        Hello docker Next Js 14.13 HELP
       </h1>
     </main>
   )
