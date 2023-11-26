cdc6c190ce46bac4db503b1849e01eed7246789d
Safi <139801311+safi-siddiqui-github@users.noreply.github.com>
2023-11-26 02:55:49 -0800

    Docker Integration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
+const nextConfig = {
+    webpack: function (config, context) {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
+        return config;
+    },
+}
 
 module.exports = nextConfig
+
+/*
+module.exports = {
+    webpackDevMiddleware: (config) => {
+      // Solve compiling problem via vagrant
+      config.watchOptions = {
+        poll: 1000,   // Check for changes every second
+        aggregateTimeout: 300,   // delay before rebuilding
+      };
+      return config;
+    }
+  };
+  */
