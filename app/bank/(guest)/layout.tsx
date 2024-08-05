378013749964acd4bbbaeef701e2f4cde1bd3db5
Safi <139801311+safi-siddiqui-github@users.noreply.github.com>
2024-08-06 01:35:00 +0500

    'update'

---
+++ app/bank/(guest)/layout.tsx
@@ -0,0 +1,16 @@
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <div className="flex h-screen">
+            <div className="w-1/2">
+                {children}
+            </div>
+            <div className="w-1/2 bg-black flex flex-col gap-10 items-center justify-center text-white text-center">
+                <div className="rounded-full w-44 h-44 bg-slate-50 animate-pulse bg-gradient-to-r from-black to-black/50"></div>
+                <div className="flex flex-col gap-1">
+                    <h2 className="text-3xl">Welcome User</h2>
+                    <p className="text-sm">Happy to seet you soon</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
+    <main className="p-5 flex flex-col gap-2">
       <h1 className="text-5xl">Next JS APP</h1>
+      <Link href={`bank`}>Bank App</Link>
     </main>
   );
 }
