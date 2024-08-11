891ffb17426258adc591c045767094f78998c171
Safi <139801311+safi-siddiqui-github@users.noreply.github.com>
2024-08-11 18:04:48 +0500

    'update'

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="p-5 flex flex-col gap-2">
-      <h1 className="text-5xl">Next JS APP</h1>
+      <h2 className="text-5xl">Next JS APP</h2>
       <Link href={`/bank`}>Bank App</Link>
       <Link href={`/linkedin`}>Linked IN App</Link>
     </main>
