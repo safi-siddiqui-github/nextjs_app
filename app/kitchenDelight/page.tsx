9494c6337aeec365f22468b7435c858da06622d7
Safi <139801311+safi-siddiqui-github@users.noreply.github.com>
2024-08-31 19:01:18 +0500

    'update'

--- app/kitchenDelight/page.tsx
+++ app/kitchenDelight/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import paratha from "@/public/kd/KD-Paratha.jpg"
+import paratha from "@/public/kd/KD-Paratha.svg"
 
 export default async function Page() {
 
@@ -17,13 +17,13 @@ export default async function Page() {
 
             <div className="flex flex-col gap-5">
 
-                <h2 className="text-xl">Breakfast</h2>
+                <h2 className="text-4xl font-semibold">Breakfast</h2>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                     {
                         bfDishes.map(({ name, price, image }) => {
                             return (
-                                <div key={name} className="flex flex-col max-w-sm md:max-w-full">
+                                <div key={name} className="flex flex-col">
                                     <Image priority={true} className="w-full rounded" src={image} alt={name} width={100} height={300} />
                                     <div className="flex flex-col">
                                         <div className="flex justify-between py-1">
